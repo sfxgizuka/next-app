c6bf6c69be4873d55eeb1b1e4ad6e5fabce52b6b
Gregory Izuka <gizuka@seamfix.com>
2024-01-28 17:24:20 +0100

    setup

---
+++ src/app/about/info/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutInfopage = () => {
+  return (
+    <div>
+        <h1 className="text-7xl">About Info page</h1>
+    </div>
+  )
+}
+
+export default AboutInfopage
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="text-7xl">Home Page</h1>
+      <Link href="/about" className="text-2xl">About Page</Link>
+    </div>
+  )
+}
+
+export default HomePage
